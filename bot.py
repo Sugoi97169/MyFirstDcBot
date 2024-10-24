4a1c05e0ef27404b255bb72b2682a94335fc9aaa
Sugoi97169 <100772741+Sugoi97169@users.noreply.github.com>
2024-10-24 08:51:34 +0800

    Update bot.py
    牌組列表改用embed形式呈現 
    調整程式架構

--- bot.py
+++ bot.py
@@ -15,33 +15,40 @@ client = commands.Bot(command_prefix="/", intents=intents)
 tz = datetime.timezone(datetime.timedelta(hours=8))
 everyday_time = datetime.time(hour=12, minute=00, tzinfo=tz)
 URL = "https://play-lh.googleusercontent.com/FNShJS-ArMjI28I4-CHlgWaA9HqKnj4DrW8-lXF2B_FH3U0KxP_djBnMuyK7Hxymxrq8"  # 動畫瘋
+ptcg_URL = "https://asia.pokemon-card.com/hk/archive/common/assets_c/2020/10/CH-TCG_logo-thumb-650xauto-15381.png" # 寶可夢牌組
 
-def poke_deck(x):
+def poke_deck_response(x):
     response = requests.get(f"https://pokecabook.com/archives/{x}")
     soup = BeautifulSoup(response.text, "html.parser")
-    pokemon = soup.find("h1").text
+    return soup
+def poke_deck_name(x):
+    pokemon = poke_deck_response(x).find("h1").text
     pokemon = pokemon.replace("環境デッキレシピまとめ", "")
+    return pokemon
+def poke_deck(x):
+    soup = poke_deck_response(x)
     results = soup.find_all("img", {"width": "800", "height": "400"}, limit=30)
     titles = soup.find_all("figcaption", {"class": "wp-element-caption"}, limit=30)
     image_links = []
     text = []
-    info =""
+    info = ""
     for result in results:
         src = result.get("src")
-        if len(image_links)==10:
+        if len(image_links) == 10:
             break
         if "http" in src:
             image_links.append(src)
     for title in titles:
-        if len(text)==10:
+        if len(text) == 10:
             break
         else:
-           text.append(title.text)
-    for i in range(0,10):
+            text.append(title.text)
+    for i in range(0, 10):
         info += f"{image_links[i]}\n{text[i]}\n-----\n"
     print(info)
     return info
-    
+
+
 def animate(x):
     info = ""
     headers = {
@@ -94,21 +101,26 @@ async def week(interaction: discord.Interaction):
     embed.set_thumbnail(url=URL)
     await interaction.response.send_message(embed=embed)
 
-@client.tree.command(name = "ptcg", description = "查詢上位牌組")
-@app_commands.describe(pokemon_name = "輸入寶可夢名稱")
+
+@client.tree.command(name="ptcg", description="查詢上位牌組")
+@app_commands.describe(pokemon_name="輸入寶可夢名稱")
 @app_commands.choices(
-        pokemon_name = [
-            Choice(name="雄偉牙",value = 2003),
-            Choice(name = "皮卡丘",value = 155139),
-            Choice(name = "忍蛙",value = 111205),
-            Choice(name = "草貓",value = 40222),
-            Choice(name = "惡噴",value = 5717),
-            Choice(name = "電蜘蛛",value = 137410),
-            Choice(name = "密勒頓",value = 2278),
-            Choice(name = "鐵荊棘",value = 111240)
-        ])
-async def ptcg( interaction: discord.Interaction, pokemon_name: Choice[int]):
-        await interaction.response.send_message(poke_deck(pokemon_name.value))
+    pokemon_name=[
+        Choice(name="雄偉牙", value=2003),
+        Choice(name="皮卡丘", value=155139),
+        Choice(name="忍蛙", value=111205),
+        Choice(name="草貓", value=40222),
+        Choice(name="惡噴", value=5717),
+        Choice(name="電蜘蛛", value=137410),
+        Choice(name="密勒頓", value=2278),
+        Choice(name="鐵荊棘", value=111240)
+    ])
+async def ptcg(interaction: discord.Interaction, pokemon_name: Choice[int]):
+    embed = discord.Embed(title=f"{poke_deck_name(pokemon_name.name)}", url="", description=poke_deck(pokemon_name.value), color=0x00ff00)
+    embed.set_author(name="狗蟻寫的寶可夢牌組查詢")
+    embed.set_thumbnail(url=ptcg_URL)
+    await interaction.response.send_message(embed=embed)
+
 
 def random_url():
     x = random.randrange(0, 1)
@@ -135,4 +147,4 @@ async def everyday():
 
 
 if __name__ == "__main__":
-    client.run('機器人金鑰')
+    client.run('金鑰')
