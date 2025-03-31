df160e1cea9c36f2d8dde0afa1f186a8fbe4244e
Sugoi97169 <100772741+Sugoi97169@users.noreply.github.com>
2025-03-31 11:13:47 +0800

    Update get_UA_card.py

--- get_UA_card.py
+++ get_UA_card.py
@@ -8,29 +8,20 @@ headers = {
 }
 icon = {
     "ico_circle_yellow": ":yellow_circle:",
-    "ico_circle_red": "red_circle",
-    "ico_circle_pruple": "pruple_circle",
-    "ico_circle_green": "green_circle",
-    "ico_circle_blue": "blue_circle"
+    "ico_circle_red": ":red_circle:",
+    "ico_circle_pruple": ":pruple_circle:",
+    "ico_circle_green": ":green_circle:",
+    "ico_circle_blue": ":blue_circle:"
 }
 def search_card(card_number):
     base_url = f'https://rugiacreation.com/ua/search?Find1={card_number}'
     r = requests.get(base_url, headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
-    if soup.find('span', {'class': 'timing'})!=None:
-        effect = soup.find('span', {'class': 'timing'}).find_parent('div')
-    elif soup.find('span', {'class': 'underline'})!=None:
-        effect = soup.find('span', {'class': 'underline'}).find_parent('div')
-    elif soup.find('span', {'class': 'times'})!=None:
-        effect = soup.find('span', {'class': 'times'}).find_parent('div')
-    elif soup.find('span', {'class': 'effectKeyword'})!=None:
-        effect = soup.find('span', {'class': 'effectKeyword'}).find_parent('div')
-    else:
-        effect = "可能有問題"
+    effect = soup.find("div",{"class": "infoBox"}).find_next_sibling('div')
     card_img = soup.find("span", {"class": "cards"}).find("img", {"class": "cards"}).get('data-src')
     card_img_link = f"https://rugiacreation.com/ua/{card_img}"
     effect_txt = ""
-    if effect != "可能有問題":
+    if effect != "":
         for node in effect.children:
             if node.name == 'img':
                 img_src = urljoin(base_url, node['src'])
@@ -42,18 +33,20 @@ def search_card(card_number):
                         effect_txt += ""
             elif hasattr(node, 'string') and node.string:
                 effect_txt += node.string.strip() + " "
+
             elif isinstance(node, str):
                 effect_txt += node.strip()
-    else:
-        effect_txt = effect
+    elif effect == "":
+        effect_txt="無效果"
+    if soup.find("span", {"class": "raidBorder"})!=None:
+        effect_txt += "突襲"+soup.find("span", {"class": "raidBorder"}).text
+
+
+
     print(card_number)
     print(card_img_link)
     print(effect_txt)
 
-for i in range(1,10):
-    card_number = f"JJK-1-00{i}" #input()
-    search_card(card_number)
-for i in range(10,101):
-    card_number = f"JJK-1-0{i}" #input()
-    search_card(card_number)
+card_number = input()
+search_card(card_number)
 
